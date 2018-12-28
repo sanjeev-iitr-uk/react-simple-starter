de4b40726fa2a1fc78555892a207a8d12bc8ef50
sanjeev_shukla <sanjeev.i.code@gmail.com>
2018-12-28 14:29:00 +0530

    version 2

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>React simple Boilerplate</div>
+      <div>React simple Boilerplate Version 2</div>
     );
   }
 }
