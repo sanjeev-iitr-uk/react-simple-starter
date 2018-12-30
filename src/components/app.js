1a869cc4d0b120a070df9c32be67389d73cf39ba
Sanjeev <sanjeev.kumar@quifers.com>
2018-12-30 17:07:05 +0530

    version 1 div added

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>React simple Boilerplate Version 2 [Version 1 Added]</div>
+        <div>
+	      <div>React simple Boilerplate Version 2 [Version 1 Added]</div>
+	      <div> code added by version 1</div>
+        </div>
     );
   }
 }
