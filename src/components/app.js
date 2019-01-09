7754ae1ee1d5b2ac1df6c43d39f3568e55de055b
sanjeev_shukla <sanjeev.i.code@gmail.com>
2019-01-09 10:27:33 +0530

    line added by version_1

--- src/components/app.js
+++ src/components/app.js
@@ -3,8 +3,7 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>New change by version_1</div>
-      <div>New line by version_1</div>
+      <p> version one line </p>
     );
   }
 }
